5cec40897378e4233dfb6e363f6f86094d814541
zhangweihu <zwh1029@outlook.com>
2017-08-15 17:16:54 +0800

    change homeScreen

--- app/screen/home/HomeScreen.js
+++ app/screen/home/HomeScreen.js
@@ -1,19 +1,42 @@
 import React, {Component} from "react";
 import {Text} from "react-native";
-import {Container, Content} from "native-base";
+import {Container, Content,Header,Title,Footer, FooterTab, Button, Left, Right, Body, Icon,View,DeckSwiper,Card,CardItem,Image,Thumbnail} from "native-base";
 //import {TitleBar} from "../../components/";
 export default class HomeScreen extends Component{
     render(){
         return(
              <Container>
-                
-                <Content>
-                    <Text>
-                        这里显示内容
-                    </Text>
-                </Content>
-              
-            </Container>
+                    <Header>
+                            <Left>
+                                <Button transparent>
+                                <Icon name='menu' />
+                                </Button>
+                            </Left>
+                            <Body>
+                                <Title >Header</Title>
+                            </Body>
+                            <Right />
+                    </Header>
+                    <Content>
+                       <Text> hello</Text>
+                    </Content>
+                    <Footer>
+                        <FooterTab>
+                            <Button active>
+                              <Icon  active name="apps" />
+                            </Button>
+                            <Button>
+                               <Icon name="camera" />
+                            </Button>
+                            <Button>
+                               <Icon  name="navigate" />
+                            </Button>
+                            <Button>
+                               <Icon name="person" />
+                            </Button>
+                        </FooterTab>
+                   </Footer>
+              </Container>
         )
     }
 }
---
+++ app/screen/home/style/HomeScreenStyle.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Function: 主页面 HomeScreen 中用到的样式
+ * Desc:
+ */
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+    Home_Title:{
+        textAlgin:center
+    }
+})
