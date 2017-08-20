fae75cc90e3f52e6d18081a54a4f925406bacf15
zhangweihu <zwh1029@outlook.com>
2017-08-20 16:12:54 +0800

    Update index.android.js

--- index.android.js
+++ index.android.js
@@ -3,5 +3,5 @@
  */
 import { AppRegistry } from "react-native";
 import AppNavigator from './app/index';
-//如果使用template 创建，需要将‘MarnoRN’替换成你自己项目的名称
+//如果使用template 创建，需要将'demo'替换成你自己项目的名称
 AppRegistry.registerComponent('demo', () => AppNavigator);
