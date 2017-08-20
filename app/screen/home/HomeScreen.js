339768b24a4eac6017d56a844cb076bd24d68189
zhangweihu <zwh1029@outlook.com>
2017-08-20 17:25:12 +0800

    network status

--- app/screen/home/HomeScreen.js
+++ app/screen/home/HomeScreen.js
@@ -1,11 +1,30 @@
 import React, {Component} from "react";
-import {Text,Navigator} from "react-native";
+import {Text,Navigator,NetInfo,StyleSheet} from "react-native";
 import {Container, Content,Header,Title,Footer,Button, Left, Right, Body, Icon,View,Card,Image,CardItem,Drawer} from "native-base";
 import {TitleBar,FootTab} from "../../components/";
 import SplashScreen from 'react-native-splash-screen'
 import DrawerScreen from '../drawer/drawerScreen'
 export default class HomeScreen extends Component{
-     componentDidMount() {
+      constructor(props){
+             super(props)
+             this.state={
+                  isConnected:null,
+                  connectionInfo:null 
+             }
+      }
+     componentDidMount() { 
+           //检查网络是否连接
+           NetInfo.isConnected.fetch().done((isConnected)=>{
+               this.setState({isConnected});
+           })
+           //检测网络连接信息
+            NetInfo.fetch().done((connectionInfo) => {
+                  this.setState({connectionInfo});
+            });
+            //监听网络变化事件
+            NetInfo.addEventListener('change', (networkType) => {
+                  this.setState({isConnected: networkType})
+            })
             SplashScreen.hide()
       }
     render(){
@@ -42,6 +61,17 @@ export default class HomeScreen extends Component{
                            <Button  block  danger onPress={()=>this.props.navigation.navigate('Scanner')}>
                                 <Text>show Scan </Text>
                            </Button>
+                           <Text style={styles.welcome}>
+                                    当前的网络状态：
+                                    {this.state.isConnected ? '网络在线': '离线'}
+                                    </Text>
+                                    <Text style={styles.welcome}>
+                                          当前网络连接类型： {this.state.connectionInfo}
+                                    </Text>
+                                    <Text style={styles.welcome}>
+                                          当前连接网络是否计费：
+                                          {NetInfo.isConnectionExpensive === true ? '需要' : '不要'}
+                           </Text>
                     </Content>
                     <Footer>
                          <Text>
@@ -57,3 +87,15 @@ export default class HomeScreen extends Component{
       this._drawer._root.open();
    }
 }
+//样式定义
+const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          paddingTop: 30
+      },
+      welcome: {
+          fontSize: 16,
+          textAlign: 'left',
+          margin: 10
+      }
+  });
